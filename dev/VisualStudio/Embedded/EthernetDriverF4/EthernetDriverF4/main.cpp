ec321ac53dcc5c8d4bd588d1aa6a3471330f02fe
Brandon Braun <brandonbraun653@gmail.com>
2019-06-02 15:28:00 -0500

    Submodule updates. Added new project for developing a custom ethernet peripheral driver.

---
+++ dev/VisualStudio/Embedded/EthernetDriverF4/EthernetDriverF4/main.cpp
@@ -0,0 +1,21 @@
+/********************************************************************************
+ * File Name:
+ *	  main.cpp
+ *
+ * Description:
+ *	  Entry point for the embedded tests of the Ethernet driver
+ *
+ * 2019 | Brandon Braun | brandonbraun653@gmail.com
+ ********************************************************************************/
+
+
+#include <Chimera/chimera.hpp>
+#include <Chimera/threading.hpp>
+
+int main( void )
+{
+  ChimeraInit();
+  
+  
+  return 0;
+}
