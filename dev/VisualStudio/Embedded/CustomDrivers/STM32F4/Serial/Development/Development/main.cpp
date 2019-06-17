e173380c92295c3d4b4b2f07ac261a75ec6bc6b3
Brandon Braun <brandonbraun653@gmail.com>
2019-06-16 21:03:42 -0500

    Added new project for STM32F4 Serial driver development. Updated submodules.

---
+++ dev/VisualStudio/Embedded/CustomDrivers/STM32F4/Serial/Development/Development/main.cpp
@@ -0,0 +1,24 @@
+/********************************************************************************
+*   File Name:
+*       main.cpp
+*       
+*   Description:
+*       Entry point for embedded development of the custom Serial (USART/UART)
+*       driver for the STM32F4 series of devices.
+*   
+*   2019 | Brandon Braun | brandonbraun653@gmail.com
+********************************************************************************/
+
+/* Thor Includes */
+#include <Thor/thor.hpp>
+
+int main()
+{
+
+  while (1) 
+  {
+    
+
+  }
+  return 0;
+}
