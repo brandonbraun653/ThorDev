7656c66024b74cef3a384c62be32448629a6b4e4
Brandon Braun <brandonbraun653@gmail.com>
2020-02-25 18:30:08 -0600

    Add a new project for testing builds on MSVC

---
+++ vs/build/MSVCStaticLibrary/MSVCStaticLibrary/entry.cpp
@@ -0,0 +1,17 @@
+/********************************************************************************
+*  File Name:
+*    entry.cpp
+*
+*  Description:
+*    Just gives the project something to compile
+*
+*  2020 | Brandon Braun | brandonbraun653@gmail.com
+********************************************************************************/
+
+/* STL Includes */
+#include <cstdint>
+
+void __do_not_use_me_only_for_compilation()
+{
+  static volatile uint32_t x = 0;
+}
