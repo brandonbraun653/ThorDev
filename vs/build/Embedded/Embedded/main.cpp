5975f9648ae0e466b685f1d8205c8b3da44c0952
Brandon Braun <brandonbraun653@gmail.com>
2020-08-30 14:46:00 -0500

    Moved embedded project to compile for STM32L432KB. Pull up submodules.

--- vs/build/Embedded/Embedded/main.cpp
+++ vs/build/Embedded/Embedded/main.cpp
@@ -17,12 +17,12 @@
 /* Chimera Includes */
 #include <Chimera/clock>
 #include <Chimera/gpio>
-#include <Chimera/pwm>
+//#include <Chimera/pwm>
 #include <Chimera/serial>
-#include <Chimera/spi>
+//#include <Chimera/spi>
 #include <Chimera/system>
 #include <Chimera/thread>
-#include <Chimera/timer>
+//#include <Chimera/timer>
 //#include <Chimera/watchdog>
 
 /* Thor Includes */
@@ -90,13 +90,13 @@ int main( void )
   blinkyThread.initialize( background_thread, nullptr, Priority::LEVEL_3, 500, "blinky" );
   blinkyThread.start();
 
-  Thread spiThread;
-  spiThread.initialize( spi_thread, nullptr, Priority::LEVEL_4, 2048, "spi" );
-  spiThread.start();
+//  Thread spiThread;
+//  spiThread.initialize( spi_thread, nullptr, Priority::LEVEL_4, 2048, "spi" );
+  //spiThread.start();
 
-  Thread pwmThread;
-  pwmThread.initialize( pwm_thread, nullptr, Priority::LEVEL_4, 2048, "pwm" );
-  pwmThread.start();
+//  Thread pwmThread;
+//  pwmThread.initialize( pwm_thread, nullptr, Priority::LEVEL_4, 2048, "pwm" );
+  //pwmThread.start();
 
   Thread serialThread;
   serialThread.initialize( serial_thread, nullptr, Priority::LEVEL_2, 2048, "serial" );
@@ -127,9 +127,9 @@ void background_thread( void *arguments )
   ledInit.threaded = false;
   ledInit.validity = true;
   ledInit.drive    = Chimera::GPIO::Drive::OUTPUT_PUSH_PULL;
-  ledInit.port     = Chimera::GPIO::Port::PORTB;
+  ledInit.port     = Chimera::GPIO::Port::PORTC;
   ledInit.pull     = Chimera::GPIO::Pull::NO_PULL;
-  ledInit.pin      = 3;
+  ledInit.pin      = 14;
 
   auto led = Chimera::GPIO::getDriver( ledInit.port );
   led->init( ledInit );
@@ -146,176 +146,176 @@ void background_thread( void *arguments )
   }
 }
 
-void spi_thread( void *arg )
-{
-  /*------------------------------------------------
-  Initialize the SPI driver
-  ------------------------------------------------*/
-  Chimera::SPI::DriverConfig init;
-
-  /* Chip select setup */
-  init.externalCS = false;
-  init.validity   = true;
-
-  init.CSInit.alternate = Chimera::GPIO::Alternate::NONE;
-  init.CSInit.drive     = Chimera::GPIO::Drive::OUTPUT_PUSH_PULL;
-  init.CSInit.pin       = 4;
-  init.CSInit.port      = Chimera::GPIO::Port::PORTA;
-  init.CSInit.pull      = Chimera::GPIO::Pull::NO_PULL;
-  init.CSInit.state     = Chimera::GPIO::State::HIGH;
-  init.CSInit.threaded  = false;
-  init.CSInit.validity  = true;
-
-  init.SCKInit.alternate = Chimera::GPIO::Alternate::SPI1_SCK;
-  init.SCKInit.drive     = Chimera::GPIO::Drive::ALTERNATE_PUSH_PULL;
-  init.SCKInit.pin       = 5;
-  init.SCKInit.port      = Chimera::GPIO::Port::PORTA;
-  init.SCKInit.threaded  = false;
-  init.SCKInit.validity  = true;
-
-  init.MISOInit.alternate = Chimera::GPIO::Alternate::SPI1_MISO;
-  init.MISOInit.drive     = Chimera::GPIO::Drive::ALTERNATE_PUSH_PULL;
-  init.MISOInit.pin       = 6;
-  init.MISOInit.port      = Chimera::GPIO::Port::PORTA;
-  init.MISOInit.threaded  = false;
-  init.MISOInit.validity  = true;
-
-  init.MOSIInit.alternate = Chimera::GPIO::Alternate::SPI1_MOSI;
-  init.MOSIInit.drive     = Chimera::GPIO::Drive::ALTERNATE_PUSH_PULL;
-  init.MOSIInit.pin       = 7;
-  init.MOSIInit.port      = Chimera::GPIO::Port::PORTA;
-  init.MOSIInit.threaded  = false;
-  init.MOSIInit.validity  = true;
-
-  init.HWInit.bitOrder    = Chimera::SPI::BitOrder::MSB_FIRST;
-  init.HWInit.clockFreq   = 1000000;
-  init.HWInit.clockMode   = Chimera::SPI::ClockMode::MODE0;
-  init.HWInit.controlMode = Chimera::SPI::ControlMode::MASTER;
-  init.HWInit.csMode      = Chimera::SPI::CSMode::AUTO_AFTER_TRANSFER;
-  init.HWInit.dataSize    = Chimera::SPI::DataSize::SZ_8BIT;
-  init.HWInit.hwChannel   = Chimera::SPI::Channel::SPI1;
-  init.HWInit.txfrMode    = Chimera::Hardware::PeripheralMode::INTERRUPT;
-  init.HWInit.validity    = true;
-
-  auto spi = Chimera::SPI::getDriver( init.HWInit.hwChannel );
-
-  volatile auto result = spi->init( init );
-
-  std::string_view test = "Hello World\r\n";
-
-  while ( 1 )
-  {
-    spi->writeBytes( test.cbegin(), test.length() );
-    Chimera::delayMilliseconds( 100 );
-  }
-}
-
-void pwm_thread( void *arg )
-{
-
-  auto red = Chimera::PWM::getDriver( 0 );
-  auto green = Chimera::PWM::getDriver( 1 );
-  auto blue = Chimera::PWM::getDriver( 2 );
-
-  Chimera::PWM::DriverConfig init;
-
-  /*------------------------------------------------
-  Initialize the Red LED Channel
-  ------------------------------------------------*/
-  init.timer.countDirection = Chimera::Timer::Direction::COUNT_UP;
-  init.timer.overwrite      = true;
-  init.timer.peripheral     = Chimera::Timer::Peripheral::TIMER2;
-  init.timer.reloadValue    = 1000;
-  init.timer.prescaler      = 80;
-  init.timer.validity       = true;
-
-  init.pwm.outputChannel = Chimera::Timer::Channel::CHANNEL_1;
-  init.pwm.compareMatch  = 100;
-  init.pwm.polarity      = Chimera::Timer::PWM::Polarity::ACTIVE_HIGH;
-  init.pwm.mode          = Chimera::Timer::PWM::Mode::EDGE_ALIGNED;
-  init.pwm.validity      = true;
-
-  init.outputPin.pin       = 0;
-  init.outputPin.port      = Chimera::GPIO::Port::PORTA;
-  init.outputPin.alternate = Chimera::GPIO::Alternate::TIM2_CH1;
-  init.outputPin.drive     = Chimera::GPIO::Drive::ALTERNATE_PUSH_PULL;
-  init.outputPin.threaded  = false;
-  init.outputPin.validity  = true;
-
-  init.validity = true;
-
-  red->init( init );
-  red->toggleOutput( false );
-
-  /*------------------------------------------------
-  Initialize the Green LED Channel
-  ------------------------------------------------*/
-  init.timer.countDirection = Chimera::Timer::Direction::COUNT_UP;
-  init.timer.overwrite      = true;
-  init.timer.peripheral     = Chimera::Timer::Peripheral::TIMER2;
-  init.timer.reloadValue    = 1000;
-  init.timer.prescaler      = 80;
-  init.timer.validity       = true;
-
-  init.pwm.outputChannel = Chimera::Timer::Channel::CHANNEL_2;
-  init.pwm.compareMatch  = 100;
-  init.pwm.polarity      = Chimera::Timer::PWM::Polarity::ACTIVE_HIGH;
-  init.pwm.mode          = Chimera::Timer::PWM::Mode::EDGE_ALIGNED;
-  init.pwm.validity      = true;
-
-  init.outputPin.pin       = 1;
-  init.outputPin.port      = Chimera::GPIO::Port::PORTA;
-  init.outputPin.alternate = Chimera::GPIO::Alternate::TIM2_CH2;
-  init.outputPin.drive     = Chimera::GPIO::Drive::ALTERNATE_PUSH_PULL;
-  init.outputPin.threaded  = false;
-  init.outputPin.validity  = true;
-
-  init.validity = true;
-
-  green->init( init );
-  green->toggleOutput( false );
-
-  /*------------------------------------------------
-  Initialize the Blue LED Channel
-  ------------------------------------------------*/
-  init.timer.countDirection = Chimera::Timer::Direction::COUNT_UP;
-  init.timer.overwrite      = true;
-  init.timer.peripheral     = Chimera::Timer::Peripheral::TIMER2;
-  init.timer.reloadValue    = 1000;
-  init.timer.prescaler      = 80;
-  init.timer.validity       = true;
-
-  init.pwm.outputChannel = Chimera::Timer::Channel::CHANNEL_4;
-  init.pwm.compareMatch  = 750;
-  init.pwm.polarity      = Chimera::Timer::PWM::Polarity::ACTIVE_HIGH;
-  init.pwm.mode          = Chimera::Timer::PWM::Mode::EDGE_ALIGNED;
-  init.pwm.validity      = true;
-
-  init.outputPin.pin       = 3;
-  init.outputPin.port      = Chimera::GPIO::Port::PORTA;
-  init.outputPin.alternate = Chimera::GPIO::Alternate::TIM2_CH4;
-  init.outputPin.drive     = Chimera::GPIO::Drive::ALTERNATE_PUSH_PULL;
-  init.outputPin.threaded  = false;
-  init.outputPin.validity  = true;
-
-  init.validity = true;
-
-  blue->init( init );
-  blue->toggleOutput( false );
-
-  while ( 1 )
-  {
-    red->toggleOutput( true );
-    green->toggleOutput( true );
-    blue->toggleOutput( true );
-    Chimera::delayMilliseconds( 1000 );
-    red->toggleOutput( false );
-    green->toggleOutput( false );
-    blue->toggleOutput( false );
-    Chimera::delayMilliseconds( 1000 );
-
-  }
-}
+//void spi_thread( void *arg )
+//{
+//  /*------------------------------------------------
+//  Initialize the SPI driver
+//  ------------------------------------------------*/
+//  Chimera::SPI::DriverConfig init;
+//
+//  /* Chip select setup */
+//  init.externalCS = false;
+//  init.validity   = true;
+//
+//  init.CSInit.alternate = Chimera::GPIO::Alternate::NONE;
+//  init.CSInit.drive     = Chimera::GPIO::Drive::OUTPUT_PUSH_PULL;
+//  init.CSInit.pin       = 4;
+//  init.CSInit.port      = Chimera::GPIO::Port::PORTA;
+//  init.CSInit.pull      = Chimera::GPIO::Pull::NO_PULL;
+//  init.CSInit.state     = Chimera::GPIO::State::HIGH;
+//  init.CSInit.threaded  = false;
+//  init.CSInit.validity  = true;
+//
+//  init.SCKInit.alternate = Chimera::GPIO::Alternate::SPI1_SCK;
+//  init.SCKInit.drive     = Chimera::GPIO::Drive::ALTERNATE_PUSH_PULL;
+//  init.SCKInit.pin       = 5;
+//  init.SCKInit.port      = Chimera::GPIO::Port::PORTA;
+//  init.SCKInit.threaded  = false;
+//  init.SCKInit.validity  = true;
+//
+//  init.MISOInit.alternate = Chimera::GPIO::Alternate::SPI1_MISO;
+//  init.MISOInit.drive     = Chimera::GPIO::Drive::ALTERNATE_PUSH_PULL;
+//  init.MISOInit.pin       = 6;
+//  init.MISOInit.port      = Chimera::GPIO::Port::PORTA;
+//  init.MISOInit.threaded  = false;
+//  init.MISOInit.validity  = true;
+//
+//  init.MOSIInit.alternate = Chimera::GPIO::Alternate::SPI1_MOSI;
+//  init.MOSIInit.drive     = Chimera::GPIO::Drive::ALTERNATE_PUSH_PULL;
+//  init.MOSIInit.pin       = 7;
+//  init.MOSIInit.port      = Chimera::GPIO::Port::PORTA;
+//  init.MOSIInit.threaded  = false;
+//  init.MOSIInit.validity  = true;
+//
+//  init.HWInit.bitOrder    = Chimera::SPI::BitOrder::MSB_FIRST;
+//  init.HWInit.clockFreq   = 1000000;
+//  init.HWInit.clockMode   = Chimera::SPI::ClockMode::MODE0;
+//  init.HWInit.controlMode = Chimera::SPI::ControlMode::MASTER;
+//  init.HWInit.csMode      = Chimera::SPI::CSMode::AUTO_AFTER_TRANSFER;
+//  init.HWInit.dataSize    = Chimera::SPI::DataSize::SZ_8BIT;
+//  init.HWInit.hwChannel   = Chimera::SPI::Channel::SPI1;
+//  init.HWInit.txfrMode    = Chimera::Hardware::PeripheralMode::INTERRUPT;
+//  init.HWInit.validity    = true;
+//
+//  auto spi = Chimera::SPI::getDriver( init.HWInit.hwChannel );
+//
+//  volatile auto result = spi->init( init );
+//
+//  std::string_view test = "Hello World\r\n";
+//
+//  while ( 1 )
+//  {
+//    spi->writeBytes( test.cbegin(), test.length() );
+//    Chimera::delayMilliseconds( 100 );
+//  }
+//}
+//
+//void pwm_thread( void *arg )
+//{
+//
+//  auto red = Chimera::PWM::getDriver( 0 );
+//  auto green = Chimera::PWM::getDriver( 1 );
+//  auto blue = Chimera::PWM::getDriver( 2 );
+//
+//  Chimera::PWM::DriverConfig init;
+//
+//  /*------------------------------------------------
+//  Initialize the Red LED Channel
+//  ------------------------------------------------*/
+//  init.timer.countDirection = Chimera::Timer::Direction::COUNT_UP;
+//  init.timer.overwrite      = true;
+//  init.timer.peripheral     = Chimera::Timer::Peripheral::TIMER2;
+//  init.timer.reloadValue    = 1000;
+//  init.timer.prescaler      = 80;
+//  init.timer.validity       = true;
+//
+//  init.pwm.outputChannel = Chimera::Timer::Channel::CHANNEL_1;
+//  init.pwm.compareMatch  = 100;
+//  init.pwm.polarity      = Chimera::Timer::PWM::Polarity::ACTIVE_HIGH;
+//  init.pwm.mode          = Chimera::Timer::PWM::Mode::EDGE_ALIGNED;
+//  init.pwm.validity      = true;
+//
+//  init.outputPin.pin       = 0;
+//  init.outputPin.port      = Chimera::GPIO::Port::PORTA;
+//  init.outputPin.alternate = Chimera::GPIO::Alternate::TIM2_CH1;
+//  init.outputPin.drive     = Chimera::GPIO::Drive::ALTERNATE_PUSH_PULL;
+//  init.outputPin.threaded  = false;
+//  init.outputPin.validity  = true;
+//
+//  init.validity = true;
+//
+//  red->init( init );
+//  red->toggleOutput( false );
+//
+//  /*------------------------------------------------
+//  Initialize the Green LED Channel
+//  ------------------------------------------------*/
+//  init.timer.countDirection = Chimera::Timer::Direction::COUNT_UP;
+//  init.timer.overwrite      = true;
+//  init.timer.peripheral     = Chimera::Timer::Peripheral::TIMER2;
+//  init.timer.reloadValue    = 1000;
+//  init.timer.prescaler      = 80;
+//  init.timer.validity       = true;
+//
+//  init.pwm.outputChannel = Chimera::Timer::Channel::CHANNEL_2;
+//  init.pwm.compareMatch  = 100;
+//  init.pwm.polarity      = Chimera::Timer::PWM::Polarity::ACTIVE_HIGH;
+//  init.pwm.mode          = Chimera::Timer::PWM::Mode::EDGE_ALIGNED;
+//  init.pwm.validity      = true;
+//
+//  init.outputPin.pin       = 1;
+//  init.outputPin.port      = Chimera::GPIO::Port::PORTA;
+//  init.outputPin.alternate = Chimera::GPIO::Alternate::TIM2_CH2;
+//  init.outputPin.drive     = Chimera::GPIO::Drive::ALTERNATE_PUSH_PULL;
+//  init.outputPin.threaded  = false;
+//  init.outputPin.validity  = true;
+//
+//  init.validity = true;
+//
+//  green->init( init );
+//  green->toggleOutput( false );
+//
+//  /*------------------------------------------------
+//  Initialize the Blue LED Channel
+//  ------------------------------------------------*/
+//  init.timer.countDirection = Chimera::Timer::Direction::COUNT_UP;
+//  init.timer.overwrite      = true;
+//  init.timer.peripheral     = Chimera::Timer::Peripheral::TIMER2;
+//  init.timer.reloadValue    = 1000;
+//  init.timer.prescaler      = 80;
+//  init.timer.validity       = true;
+//
+//  init.pwm.outputChannel = Chimera::Timer::Channel::CHANNEL_4;
+//  init.pwm.compareMatch  = 750;
+//  init.pwm.polarity      = Chimera::Timer::PWM::Polarity::ACTIVE_HIGH;
+//  init.pwm.mode          = Chimera::Timer::PWM::Mode::EDGE_ALIGNED;
+//  init.pwm.validity      = true;
+//
+//  init.outputPin.pin       = 3;
+//  init.outputPin.port      = Chimera::GPIO::Port::PORTA;
+//  init.outputPin.alternate = Chimera::GPIO::Alternate::TIM2_CH4;
+//  init.outputPin.drive     = Chimera::GPIO::Drive::ALTERNATE_PUSH_PULL;
+//  init.outputPin.threaded  = false;
+//  init.outputPin.validity  = true;
+//
+//  init.validity = true;
+//
+//  blue->init( init );
+//  blue->toggleOutput( false );
+//
+//  while ( 1 )
+//  {
+//    red->toggleOutput( true );
+//    green->toggleOutput( true );
+//    blue->toggleOutput( true );
+//    Chimera::delayMilliseconds( 1000 );
+//    red->toggleOutput( false );
+//    green->toggleOutput( false );
+//    blue->toggleOutput( false );
+//    Chimera::delayMilliseconds( 1000 );
+//
+//  }
+//}
 
 void serial_thread( void *arg )
 {
@@ -374,11 +374,11 @@ void serial_thread( void *arg )
   /*------------------------------------------------
   Test string to transmit
   ------------------------------------------------*/
-  std::string_view hello = "hello\r\n";
+  std::string_view hello = "hello world\r\n";
 
   while ( true )
   {
     serial->write( reinterpret_cast<const uint8_t *>( hello.data() ), hello.size() );
-    Chimera::delayMilliseconds( 100 );
+    Chimera::delayMilliseconds( 1000 );
   }
 }
