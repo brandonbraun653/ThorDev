248ecda7f0f839ea841a78c2378c6af00b1b679e
brandonbraun653 <brandonbraun653@gmail.com>
2020-11-04 16:13:34 -0600

    Pull up submodules. Add hardware test for LLD EXTI driver

--- tests/hardware/integration/can/can_driver_integration_tests.py
+++ tests/hardware/integration/can/can_driver_integration_tests.py
@@ -9,7 +9,6 @@
 # **********************************************************************************************************************
 
 import can
-import unittest
 
 
 def execute_test_suite() -> None:
@@ -17,14 +16,13 @@ def execute_test_suite() -> None:
     Executes the test suite
 
     This requires setting up the SocketCan interface properly using the below commands:
-        sudo ip link set can0 type can bitrate 1000000
+        sudo ip link set can0 type can bitrate 5000000
         sudo ifconfig can0 up
 
     :return: None
     """
 
-    #can0 = can.interface.Bus(channel='can0', bustype='socketcan_ctypes')
-    can0 = can.interface.Bus(bustype='slcan', channel='/dev/ttyACM0', bitrate=500000)
+    can0 = can.interface.Bus(channel='can0', bustype='socketcan_ctypes')
     msg = can.Message(arbitration_id=0x49, data=[0, 1, 2, 3, 4, 5, 6, 7], extended_id=False)
 
     try:
---
+++ tests/hardware/peripherals/stm32l4/exti/test_driver_interface.cpp
@@ -0,0 +1,171 @@
+/********************************************************************************
+ *  File Name:
+ *    test_driver_interface.cpp
+ *
+ *  Description:
+ *    Tests the STM32L4 LLD EXTI Interface
+ *
+ *  2020 | Brandon Braun | brandonbraun653@gmail.com
+ *******************************************************************************/
+
+/* STL Includes */
+#include <limits>
+
+/* Chimera Includes */
+#include <Chimera/exti>
+
+/* Thor Includes */
+#include <Thor/lld/common/types.hpp>
+#include <Thor/lld/interface/exti/exti_detail.hpp>
+#include <Thor/lld/interface/exti/exti_intf.hpp>
+#include <Thor/lld/interface/exti/exti_prv_data.hpp>
+#include <Thor/lld/interface/exti/exti_types.hpp>
+
+#include <Thor/lld/stm32l4x/exti/hw_exti_prj.hpp>
+#include <Thor/lld/stm32l4x/exti/hw_exti_types.hpp>
+
+/* CppUTest Includes */
+#include <CppUTest/TestHarness.h>
+
+/*-------------------------------------------------------------------------------
+Aliases
+-------------------------------------------------------------------------------*/
+namespace LLD = Thor::LLD::EXTI;
+
+/*-------------------------------------------------------------------------------
+Constants
+-------------------------------------------------------------------------------*/
+static constexpr size_t MIN_GPIO_EXTI_LINE = 0;
+static constexpr size_t MAX_GPIO_EXTI_LINE = 15;
+
+/*-------------------------------------------------------------------------------
+Static Functions & Data
+-------------------------------------------------------------------------------*/
+static bool sISRFired = false;
+
+static void markIsrFired( void *arg )
+{
+  sISRFired = true;
+}
+
+/*-------------------------------------------------------------------------------
+Testing Functions
+-------------------------------------------------------------------------------*/
+TEST_GROUP( STM32L4_LLD_EXTI_INTF ){};
+
+/*-------------------------------------------------------------------------------
+Public Project Implementation
+-------------------------------------------------------------------------------*/
+TEST( STM32L4_LLD_EXTI_INTF, NumInterruptLines )
+{
+  CHECK( LLD::numInterruptLines() == LLD::NUM_EXTI_LINES );
+}
+
+
+TEST( STM32L4_LLD_EXTI_INTF, OpenCloseOpenClose )
+{
+  CHECK( LLD::open() == Chimera::Status::OK );
+  CHECK( LLD::close() == Chimera::Status::OK );
+
+  CHECK( LLD::open() == Chimera::Status::OK );
+  CHECK( LLD::open() == Chimera::Status::OK );
+
+  CHECK( LLD::close() == Chimera::Status::OK );
+  CHECK( LLD::close() == Chimera::Status::OK );
+}
+
+
+// TEST( STM32L4_LLD_EXTI_INTF, SoftwareTriggerISR )
+// {
+//   /*-------------------------------------------------
+//   Initialize the test
+//   -------------------------------------------------*/
+//   LLD::open();
+
+//   /*-------------------------------------------------
+//   Exercise every GPIO line that can be triggered with
+//   a software interrupt. Edge triggering doesn't
+//   matter in this test.
+//   -------------------------------------------------*/
+//   for( size_t line = MIN_GPIO_EXTI_LINE; line < MAX_GPIO_EXTI_LINE; line++ )
+//   {
+//     /*-------------------------------------------------
+//     Attach the interrupt callback
+//     -------------------------------------------------*/
+//     sISRFired = false;
+//     CHECK( LLD::attach( line, Chimera::EXTI::EdgeTrigger::RISING_EDGE, markIsrFired ) == Chimera::Status::OK );
+
+//     /*-------------------------------------------------
+//     Verify the flag was set to true via the callback
+//     -------------------------------------------------*/
+//     CHECK( LLD::trigger( line ) );
+//     Chimera::delayMilliseconds( 5 );
+//     CHECK( sISRFired == true );
+
+//     /*-------------------------------------------------
+//     Detach the interrupt from firing
+//     -------------------------------------------------*/
+//     CHECK( LLD::detach( line ) == Chimera::Status::OK );
+
+//     /*-------------------------------------------------
+//     Verify it cannot fire again
+//     -------------------------------------------------*/
+//     sISRFired = false;
+//     CHECK( LLD::trigger( line ) );
+//     Chimera::delayMilliseconds( 5 );
+//     CHECK( sISRFired == false );
+//   }
+
+//   /*-------------------------------------------------
+//   Destroy the test
+//   -------------------------------------------------*/
+//   LLD::close();
+// }
+
+
+TEST( STM32L4_LLD_EXTI_INTF, EnableDisable )
+{
+  /*-------------------------------------------------
+  Initialize the test
+  -------------------------------------------------*/
+  LLD::open();
+
+  /*-------------------------------------------------
+  Exercise every GPIO line that can be triggered with
+  a software interrupt. Edge triggering doesn't
+  matter in this test.
+  -------------------------------------------------*/
+  for( size_t line = MIN_GPIO_EXTI_LINE; line < MAX_GPIO_EXTI_LINE; line++ )
+  {
+    /*-------------------------------------------------
+    Attach the interrupt callback and disable (mask)
+    the signal from firing.
+    -------------------------------------------------*/
+    sISRFired = false;
+    CHECK( LLD::attach( line, Chimera::EXTI::EdgeTrigger::RISING_EDGE, markIsrFired ) == Chimera::Status::OK );
+    CHECK( LLD::disable( line ) == Chimera::Status::OK );
+
+    /*-------------------------------------------------
+    Verify the flag was set to true via the callback
+    only after the line is re-enabled.
+    -------------------------------------------------*/
+    CHECK( LLD::trigger( line ) );
+    Chimera::delayMilliseconds( 5 );
+    CHECK( sISRFired == false );
+
+    CHECK( LLD::enable( line ) == Chimera::Status::OK );
+    CHECK( LLD::trigger( line ) );
+    Chimera::delayMilliseconds( 5 );
+    CHECK( sISRFired == true );
+
+    /*-------------------------------------------------
+    Destroy the interrupt config
+    -------------------------------------------------*/
+    CHECK( LLD::detach( line ) == Chimera::Status::OK );
+  }
+
+  /*-------------------------------------------------
+  Destroy the test
+  -------------------------------------------------*/
+  LLD::close();
+}
