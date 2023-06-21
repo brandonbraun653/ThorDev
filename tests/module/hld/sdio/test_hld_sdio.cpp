386867a6e58c5fba69b5c549b931d5edd7638799
Brandon Braun <brandonbraun653@protonmail.com>
2023-06-20 21:35:32 -0700

    Sync today's work on the SDIO driver

--- tests/module/hld/sdio/test_hld_sdio.cpp
+++ tests/module/hld/sdio/test_hld_sdio.cpp
@@ -11,19 +11,55 @@
 /*-----------------------------------------------------------------------------
 Includes
 -----------------------------------------------------------------------------*/
+#include <Chimera/sdio>
+
 #include "CppUTest/TestHarness.h"
 #include "CppUTest/CommandLineTestRunner.h"
+#include "CppUTestExt/MockSupport.h"
 
-int main(int ac, char** av)
+/*-----------------------------------------------------------------------------
+Static Data
+-----------------------------------------------------------------------------*/
+
+/*-----------------------------------------------------------------------------
+Public Functions
+-----------------------------------------------------------------------------*/
+int main( int ac, char **av )
 {
-   return CommandLineTestRunner::RunAllTests(ac, av);
+  return CommandLineTestRunner::RunAllTests( ac, av );
 }
 
-TEST_GROUP(FirstTestGroup)
+namespace Thor::LLD::SDIO
 {
+  bool isSupported( const Chimera::SDIO::Channel channel )
+  {
+    mock().actualCall( "Thor::LLD::SDIO::isSupported" )
+      .withParameterOfType( "Chimera::SDIO::Channel", "channel", &channel )
+      .returnBoolValueOrDefault( false );
+
+    return mock().boolReturnValue();
+  }
+}
+
+/*-----------------------------------------------------------------------------
+Test High Level User Interface
+-----------------------------------------------------------------------------*/
+TEST_GROUP( UserInterface )
+{
+  void setup()
+  {
+  }
+
 };
 
-TEST(FirstTestGroup, FirstTest)
+TEST( UserInterface, Initialize )
 {
-   CHECK_EQUAL(1, 1);
+  mock().actualCall( "Thor::LLD::SDIO::isSupported" )
+    .withParameter( "channel", Chimera::SDIO::Channel::SDIO1 )
+    .andReturnValue( true );
+
+  Chimera::Status_t result = Chimera::SDIO::initialize();
+
+  mock().checkExpectations();
+  CHECK_EQUAL( Chimera::Status::NOT_SUPPORTED, result );
 }
