165fedd7151ba1c1f80a2d2604e61d8f0b4542f9
Brandon Braun <brandonbraun653@protonmail.com>
2023-08-16 21:02:51 -0700

    Add more tests

--- tests/integration/sdio/sdio_integration_test.cpp
+++ tests/integration/sdio/sdio_integration_test.cpp
@@ -1,6 +1,6 @@
 /******************************************************************************
  *  File Name:
- *    test_lld_sdio.cpp
+ *    sdio_integration_test.cpp
  *
  *  Description:
  *    Integration testing the Thor SDIO driver
@@ -21,11 +21,11 @@ Includes
 /*-----------------------------------------------------------------------------
 Constants
 -----------------------------------------------------------------------------*/
-#define TestChannel     ( Chimera::SDIO::Channel::SDIO1 )
-#define TestPeriph      ( Thor::LLD::SDIO::SDIO1_PERIPH )
-#define TestBlockCount  ( 5u )
-#define TestBlockSize   ( 512u )
-#define TestArrCount    ( TestBlockCount * ( TestBlockSize / sizeof( uint32_t ) ) )
+#define TestChannel    ( Chimera::SDIO::Channel::SDIO1 )
+#define TestPeriph     ( Thor::LLD::SDIO::SDIO1_PERIPH )
+#define TestBlockCount ( 5u )
+#define TestBlockSize  ( 512u )
+#define TestArrCount   ( TestBlockCount * ( TestBlockSize / sizeof( uint32_t ) ) )
 
 /*-----------------------------------------------------------------------------
 Variables
@@ -48,7 +48,7 @@ Test Group: Integration Tests (requires SD card connection)
 TEST_GROUP( IntegrationTests )
 {
   Chimera::SDIO::Driver_rPtr sdio;
-  Chimera::SDIO::HWConfig cfg;
+  Chimera::SDIO::HWConfig    cfg;
 
   void setup()
   {
@@ -84,6 +84,14 @@ TEST_GROUP( IntegrationTests )
   {
     sdio->close();
     sdio = nullptr;
+  }
+
+  void fill_tx_buffer_with_random_data()
+  {
+    for ( size_t i = 0; i < ( TestBlockSize / sizeof( uint32_t ) ); i++ )
+    {
+      TestTXBuffer[ i ] = static_cast<uint32_t>( rand() );
+    }
   }
 };
 
@@ -145,7 +153,11 @@ TEST( IntegrationTests, SingleBlockDataIntegrity )
   }
 
   CHECK( sdio->writeBlock( 0, 1, TestTXBuffer, TestBlockSize ) == Chimera::Status::OK );
-  Chimera::delayMilliseconds( 150 );
+
+  while ( sdio->getCardState() != Chimera::SDIO::CardState::CARD_READY )
+  {
+    Chimera::delayMilliseconds( 10 );
+  }
 
   /*---------------------------------------------------------------------------
   Read the data back and check for equality
@@ -156,8 +168,8 @@ TEST( IntegrationTests, SingleBlockDataIntegrity )
 
 TEST( IntegrationTests, MultiBlockDataIntegrity )
 {
-  constexpr size_t NUM_BLOCKS = 5;
-  constexpr size_t TEST_SIZE = NUM_BLOCKS * TestBlockSize;
+  constexpr size_t NUM_BLOCKS = TestBlockCount;
+  constexpr size_t TEST_SIZE  = NUM_BLOCKS * TestBlockSize;
 
   /*---------------------------------------------------------------------------
   Connect to the device
@@ -168,17 +180,118 @@ TEST( IntegrationTests, MultiBlockDataIntegrity )
   /*---------------------------------------------------------------------------
   Write a unique set of data to the card
   ---------------------------------------------------------------------------*/
-  for ( size_t i = 0; i < ( TestBlockSize / sizeof( uint32_t ) ); i++ )
-  {
-    TestTXBuffer[ i ] = static_cast<uint32_t>( rand() );
-  }
-
+  fill_tx_buffer_with_random_data();
   CHECK( sdio->writeBlock( 0, NUM_BLOCKS, TestTXBuffer, TEST_SIZE ) == Chimera::Status::OK );
-  Chimera::delayMilliseconds( 300 );
+
+  while ( sdio->getCardState() != Chimera::SDIO::CardState::CARD_READY )
+  {
+    Chimera::delayMilliseconds( 10 );
+  }
 
   /*---------------------------------------------------------------------------
   Read the data back and check for equality
   ---------------------------------------------------------------------------*/
   CHECK( sdio->readBlock( 0, NUM_BLOCKS, TestRXBuffer, TEST_SIZE ) == Chimera::Status::OK );
   CHECK( 0 == memcmp( TestTXBuffer, TestRXBuffer, TEST_SIZE ) );
+}
+
+TEST( IntegrationTests, SingleBlockErase )
+{
+  /*-------------------------------------------------------------------------
+  Connect to the device
+  -------------------------------------------------------------------------*/
+  CHECK( sdio->open( cfg ) == Chimera::Status::OK );
+  CHECK( sdio->connect() == Chimera::Status::OK );
+
+  /*-------------------------------------------------------------------------
+  Write a unique set of data to the card
+  -------------------------------------------------------------------------*/
+  fill_tx_buffer_with_random_data();
+  CHECK( sdio->writeBlock( 0, 1, TestTXBuffer, TestBlockSize ) == Chimera::Status::OK );
+
+  while ( sdio->getCardState() != Chimera::SDIO::CardState::CARD_READY )
+  {
+    Chimera::delayMilliseconds( 10 );
+  }
+
+  /*-------------------------------------------------------------------------
+  Erase the block
+  -------------------------------------------------------------------------*/
+  CHECK( sdio->eraseBlock( 0, 1 ) == Chimera::Status::OK );
+
+  while ( sdio->getCardState() != Chimera::SDIO::CardState::CARD_READY )
+  {
+    Chimera::delayMilliseconds( 10 );
+  }
+
+  /*-------------------------------------------------------------------------
+  Read the data back and check for erasure
+  -------------------------------------------------------------------------*/
+  memset( TestRXBuffer, 0, sizeof( TestRXBuffer ) );
+
+  CHECK( sdio->readBlock( 0, 1, TestRXBuffer, TestBlockSize ) == Chimera::Status::OK );
+  CHECK( 0 == memcmp( TestTXBuffer, TestRXBuffer, TestBlockSize ) );
+}
+
+
+TEST( IntegrationTests, MultiBlockErase )
+{
+  constexpr size_t NUM_BLOCKS = TestBlockCount;
+  constexpr size_t TEST_SIZE  = NUM_BLOCKS * TestBlockSize;
+
+  /*-------------------------------------------------------------------------
+  Connect to the device
+  -------------------------------------------------------------------------*/
+  CHECK( sdio->open( cfg ) == Chimera::Status::OK );
+  CHECK( sdio->connect() == Chimera::Status::OK );
+
+  /*-------------------------------------------------------------------------
+  Write a unique set of data to the card
+  -------------------------------------------------------------------------*/
+  fill_tx_buffer_with_random_data();
+  CHECK( sdio->writeBlock( 0, NUM_BLOCKS, TestTXBuffer, TEST_SIZE ) == Chimera::Status::OK );
+
+  while ( sdio->getCardState() != Chimera::SDIO::CardState::CARD_READY )
+  {
+    Chimera::delayMilliseconds( 10 );
+  }
+
+  /*-------------------------------------------------------------------------
+  Erase the block
+  -------------------------------------------------------------------------*/
+  CHECK( sdio->eraseBlock( 0, NUM_BLOCKS ) == Chimera::Status::OK );
+
+  while ( sdio->getCardState() != Chimera::SDIO::CardState::CARD_READY )
+  {
+    Chimera::delayMilliseconds( 10 );
+  }
+
+  /*-------------------------------------------------------------------------
+  Read the data back and check for erasure
+  -------------------------------------------------------------------------*/
+  memset( TestRXBuffer, 0, sizeof( TestRXBuffer ) );
+
+  CHECK( sdio->readBlock( 0, NUM_BLOCKS, TestRXBuffer, TEST_SIZE ) == Chimera::Status::OK );
+  CHECK( 0 == memcmp( TestTXBuffer, TestRXBuffer, TestBlockSize ) );
+}
+
+TEST( IntegrationTests, RetrieveCardStatus )
+{
+  /*-----------------------------------------------------------------------------
+  Connect to the device
+  -----------------------------------------------------------------------------*/
+  CHECK( sdio->open( cfg ) == Chimera::Status::OK );
+  CHECK( sdio->connect() == Chimera::Status::OK );
+
+  /*-------------------------------------------------------------------------
+  Retrieve the card status
+  -------------------------------------------------------------------------*/
+  Chimera::SDIO::CardStatus status;
+
+  CHECK( sdio->getCardStatus( status ) == Chimera::Status::OK );
+}
+
+TEST( IntegrationTests, CardDependentReadWrite )
+{
+  // TODO: Use the detected card information to perform reads/writes across a wide address range
 }
